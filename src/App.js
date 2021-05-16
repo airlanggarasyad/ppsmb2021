8412841a2ca2575c7cd4584e153e812200ea89b4
DESKTOP-M0KPJV6\arudo <arudo.saka@gmail.com>
2021-05-16 21:16:46 +0700

    [16 May 2021 21:16] Testing breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-import Galeri from './pages/Galeri'
+import Galeri from "./pages/Galeri";
 import GlobalStyle from "./globalStyle";
+import Footer from "./components/Footer";
+import test1 from "./pages/test1";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Router>
         <GlobalStyle />
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Galeri} />
-          </Switch>
-        </Router>
-      </>
+        <Switch>
+          <Route exact path="/" component={Galeri} />
+          <Route exact path="/test1" component={test1}></Route>
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
 }
--- src/pages/Galeri.js
+++ src/pages/Galeri.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
-import Footer from '../components/Footer'
+import React, { Component } from "react";
+import Breadcrumbs from "../components/Breadcrumbs";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
+import test1 from './test1'
 
 export default class Galeri extends Component {
-    render() {
-        return (
-            <div>
-                <Footer/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Breadcrumbs />
+        <Link to="/test1">Hari 1</Link>
+        <Switch>
+        </Switch>
+      </div>
+    );
+  }
 }
