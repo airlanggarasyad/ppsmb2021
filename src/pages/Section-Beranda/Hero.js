bda8b191d766ccd6b7aa8e1016377c93eddff198
chrstnale <christianale85@gmail.com>
2021-05-30 10:17:18 +0700

    [30 May 2021 10:16] Modified hero section to 100vh height

--- src/pages/Section-Beranda/Hero.js
+++ src/pages/Section-Beranda/Hero.js
@@ -2,11 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 import mainBG from "../../assets/img/main-bg.webp";
-import Logo from "../../assets/img/logo-UGM-PPSMB.png";
 import LogoPPSMB from "../../assets/img/logo-ppsmb-decorative.svg";
-import LowerCorner from "../../assets/img/decorative-cornerL-1.svg";
-import UpperLeft from "../../assets/img/decorative-cornerL-3.webp";
-import UpperRight from "../../assets/img/decorative-cornerL-2.webp";
+import UpperLeft from "../../assets/img/corner-bulk1.webp";
+import UpperRight from "../../assets/img/corner-bulk2.webp";
 import Batik from "../../assets/img/batik-color.svg";
 import Sliders from "../../components/Slider/Slider";
 
@@ -14,40 +12,29 @@ export default function Hero() {
     return (
         <Container>
 
-            <div className='top'>
-                <div className='top-left'>
-                    <img src={UpperLeft} alt="" srcset="" className="corner"/>
-                </div>
-                <div className='top-center'>
-                    <img src={Logo} alt="" srcset=""/>
-                </div>
-                <div className='top-right'>
-                    <img src={UpperRight} alt="" srcset="" className="corner"/>
-                </div>
+            <div className='corner-decoratives'>
+                <img src={UpperLeft} alt="" srcset="" className="corner upper-left"/>
+                <img src={UpperRight} alt="" srcset="" className="corner upper-right"/>
             </div>
 
-            <div className='bottom'>
-
-                <div className='bottom-left'>
-                    <div className='logo'>
-                        <img src={LogoPPSMB} alt="" srcset="" className="logo-decorative"/>
-                    </div>
+            <div className='main'>
+                <div className='hero-logo'>
+                    <img src={LogoPPSMB} alt="Logo Dekoratif PPSMB 2021" srcset=""/>
                 </div>
 
-                <div className='bottom-right'>
-                    <div className='bottom-right-up'>
-                        <div className='title'>
-                            <h2 className="text-center m-0 ppsmb ppsmb-darkblue">
+                <div className='hero-content'>
+                    <div className='content-title'>
+                        <h1 className="text-center m-0 ppsmb ppsmb-darkblue">
                                 PPSMB UGM
                                 <span className="ppsmb-blue"> 2</span>
                                 <span className="ppsmb-red">0</span>
                                 <span className="ppsmb-orange">2</span>
                                 <span className="ppsmb-yellow">1</span>
-                            </h2>
                             <h1 className="text-center m-0 font-indonesia-script ppsmb-darkblue">
-                                Temanya apa ya
+                                Yang Penting Hepi
                             </h1>
-                        </div>
+                        </h1>
+                       
                         <div className='batiks'>
                             <img src={Batik} alt="" srcset="" className="batik"/>
                             <img src={Batik} alt="" srcset="" className="batik"/>
@@ -57,17 +44,34 @@ export default function Hero() {
                     </div>
                     <div className='lini-masa text-center ppsmb-darkblue'>
                         <h2>Lini Masa</h2>
-                        <Sliders slidesToShow='1'>
-                            <div>1</div>
-
-                            <div>2</div>
-
-                            <div>3</div>
-                            <div>4</div>
-
-                            <div>5</div>
-
-                            <div>6</div>
+                        <Sliders 
+                        slidesToShow={3}
+                        slidesToShowMobile={2}
+                        slidesToShowTablet={2}>
+                            <div>
+                                <Card><p>1</p></Card>
+                            </div>                                
+                            <div>
+                                <Card><p>2</p></Card>
+                            </div>
+                            <div>
+                                <Card><p>3</p></Card>
+                            </div>                                
+                            <div>
+                                <Card><p>4</p></Card>
+                            </div>
+                            <div>
+                                <Card><p>5</p></Card>
+                            </div>                                
+                            <div>
+                                <Card><p>6</p></Card>
+                            </div>
+                            <div>
+                                <Card><p>7</p></Card>
+                            </div>                        
+                            <div>
+                                <Card><p>8</p></Card>
+                            </div>
                         </Sliders>
                     </div>
                 </div>
@@ -83,122 +87,90 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     margin:0;
-    background-size: cover;
+    box-sizing: border-box;
+    justify-content: center;
 
-    .corner{
-        width: 30vmin;
+    background-image: url(${mainBG});
+    background-position: center;
+    background-size: cover;
+    overflow: hidden;
+
+    height: 100vh;
+
+    z-index: 1;
+    
+    img{
         pointer-events: none;
     }
 
-    .top{
-        display: flex;
-        justify-content: space-between;
-    }
-    .top-left{
-        /* background-color: red; */
-    }
-    .top-center{
-        /* background-color: green; */
-        margin-top: 5vmin;
-        img {
-            width: 28vmin;
-            pointer-events: none;
-        }
-    }
-    .top-right{
-        /* background-color: blue; */
+    .corner-decoratives{
+        position: relative;
+        height: 20%;
     }
 
-    .bottom {
+    .upper-left {
+        position: absolute;
+        transform: translateY(-100%) rotate(90deg);
+        transform-origin: left bottom;
+        top: 0;
+        left: 0;
+    }
+    .upper-right {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+
+    .main {
         display: flex;
         flex-direction: row;
-        
+        height: 80%;
+        justify-content: space-between;
     }
 
-    .bottom-left{
+    .hero-logo{
         width: 40vw;
         justify-content: flex-end;
-        /* background-color: red; */
-    }
-    
-    .logo-decorative{
-      /* /* margin-left: -10%;
-      grid-column: 1 / span 5;
-      grid-row: 2 / span 5;
-      z-index: 0; */
 
-        margin-left: -10vw;
-        margin-bottom: -20vh;
-        width: 45vw;
-
-        pointer-events: none;
+        img{
+            margin-left: -10%;
+            width: 103%;
+        }
     }
 
     
-    .bottom-right{
-        /* background-color: blue; */
-        width: 60%;
+    .hero-content{
+        width: 60vw;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    @media (max-width: 576px) {
-        .bottom {
-            flex-direction: column;
-            /* width:400px; */
-        }
-        .bottom-right {
-            order: 1;
-            width: 100%;
-        }
-        .bottom-left {
-            order: 2;
-            width: 100vw;
-
-        }
-
-        .logo-decorative {
-            margin-left: -30vmin;
-            margin-bottom: -85vmin;
-            width: 100vmin;
-        }
-        
+        align-items: flex-start;
     }
 
 
 
-    .bottom-right-up{
+    .content-title{
         display: flex;
         flex-direction: row;
         width: 100%;
         justify-content: center;
         align-items: center;
     }
-    .title{
-        white-space: nowrap;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        /* background-color: green; */
-    }
 
     
     .ppsmb {
-    font-size: calc(0.5rem + 3vw);
-    text-align: right;
-    /* animation: 1s ease-in-out 0s 1 fadeIn;
-    animation-delay: calc(var(--delay-multiplier) * 3);  
-    animation-fill-mode: forwards; */
+    text-align: center;
+    font-size: calc(0.5rem + 2.5vmin);
+
     }
 
   .font-indonesia-script {
     opacity:0;
-    font-size: calc(0.5rem + 4vw);
-    line-height: calc(0.5rem + 7vw);
+    font-size: calc(0.5rem + 7.5vmin);
+    line-height: calc(0.5rem + 9vmin);
+    margin-top: calc(0.5rem + 2vmin);
+    font-weight: lighter;
 
-    text-align: right;
+    text-align: center;
 
     animation: 1s ease-in-out 0s 1 fadeIn;
     animation-delay: calc(var(--delay-multiplier) * 5);  
@@ -206,26 +178,68 @@ const Container = styled.div`
   }
 
   .batiks{
-      /* background-color: yellow; */
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-top: calc(0.5rem + 2vmin);
+    margin-left: 5vw;
 
-      pointer-events: none;
-      
   }
   
   .batik{
-      padding-left: calc(0.5rem + 7vw);
-      height: calc(0.5rem + 3vw);
+      padding: 1vmin;
+      height: calc(0.5rem + 4vmin);
   }
 
   .lini-masa {
-      /* background-color: aqua; */
-    margin: 0;
     width: 100%;
-    height: 50%;
     font-size: calc(0.5rem + 1.2vmin)
   }
+  @media (max-width: 1024px) {
+      .corner-decoratives{
+          height: 35%;
+      }
+      .main{
+          height: 65%;
+      }
+  }
+
+
+  @media (max-width: 576px) {
+        .main {
+            flex-direction: column;
+            height: 80%;
+        }
+        .hero-logo {
+            order: 2;
+            width: 70vw;
+        }
+        .hero-content{
+            width: 100vw;
+            justify-content: flex-start;
+        }
+
+        .content-title {
+            flex-direction: column;
+        }
+
+        .batiks {
+            flex-direction: row;
+        }
+        
+    }
+
+
+`
+
+const Card = styled.div`
+    box-sizing: border-box;
+    float: center;
+    background-color:grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20vmin;
+    margin: 10px;
+
+
 `
