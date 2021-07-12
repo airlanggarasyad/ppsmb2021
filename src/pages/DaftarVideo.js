d4a5f7fd7832d80f0d6acfffb2d754c615fa3d05
chrstnale <christianale85@gmail.com>
2021-07-12 15:23:45 +0700

    [12 July 2021 15:23] Modified copywriting and animation

--- src/pages/DaftarVideo.js
+++ src/pages/DaftarVideo.js
@@ -1,13 +1,13 @@
 export const DaftarVideo = [
     {
         title: 'Apa Itu PPSMB?',
-        desc: 'Pahami apa itu PPSMB di sini!',
+        desc: 'Yuk, kenali lebih dalam tentang PPSMB UGM!',
         embedId: '_3uPoHK0XZk',
         thumbnail: require('../assets/img/serba-serbi/apaituppsmb.webp').default,
     },
     {
         title: 'Tarian Selebrasi - PPSMB UGM 2021',
-        desc: 'Sudahkah Gamada menghafalkan lirik Anthem "Sinergi dalam Harmoni"?',
+        desc: 'Sudahkah Gamada menghafalkan tarian selebrasi PPSMB UGM 2021?',
         embedId: 'AAtiYhMi_4o',
         thumbnail: require('../assets/img/serba-serbi/tarianselebrasi.webp').default,
     },
